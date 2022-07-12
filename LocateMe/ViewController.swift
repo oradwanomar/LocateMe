6d5e5a15e16d8f4466c511a92b22b5035c3384a9
omarradwan <oradwan037@gmail.com>
2022-07-12 17:40:00 +0200

    addAnnotation

--- LocateMe/ViewController.swift
+++ LocateMe/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let initLocation = CLLocation(latitude: 24.69371, longitude: 46.723596)
         setStartingLocation(location: initLocation, distance: 10000)
+        addAnnotation()
     }
     
     func setStartingLocation(location: CLLocation,distance: CLLocationDistance){
@@ -22,8 +23,16 @@ class ViewController: UIViewController {
         mapView.setRegion(region, animated: true)
         mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
         
-        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 100)
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 1000)
         mapView.setCameraZoomRange(zoomRange, animated: true)
+    }
+    
+    func addAnnotation(){
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2D(latitude: 24.69371, longitude: 46.723596)
+        pin.title = "My title"
+        pin.subtitle = "My syb title"
+        mapView.addAnnotation(pin)
     }
 
 
