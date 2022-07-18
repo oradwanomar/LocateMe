80cb96e4e3c8a0d447f15d377ba39013a23fe4bb
omarradwan <oradwan037@gmail.com>
2022-07-18 08:57:29 +0200

    edit

--- LocateMe/MyLocationVC.swift
+++ LocateMe/MyLocationVC.swift
@@ -32,7 +32,6 @@ class MyLocationVC: UIViewController, CLLocationManagerDelegate {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
     }
     
-
     
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
